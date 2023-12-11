cd0354618794d813dd25f01f73e843b621a05cd2
natashamalynicz <natashamalynicz@gmail.com>
2023-12-11 19:53:21 -0300

    first commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const productos [
     titulo: "iphone12";
     imagen:"./img/iphone-12.png";
     categoria:{
-        nombre: "iphone 12"
+        nombre: "iphone 12",
         id: "iphone 12"
     },
     precio: 1000;
@@ -75,17 +75,17 @@ const productos [
              ]
 
              const contenedorProductos = document.querySelector ("#contenedor.productos");
-             const botonesCategorias = document.querySelector (".botonc");
+             const botonesCategorias = document.querySelectorAll (".botonc");
 
              function cargarProductos() {
               productos.forEach (producto => {
                 const div = document.createElement("div");
                 div.classList.add ("producto"),
                 div.innerHTML = `
-                img class="producto-imagen" src="${producto.imagen} alt="imagencelular">
+                img class="producto-imagen" src="${producto-imagen} alt="${producto-titulo}">
                 <div class="caracteristicas" >
-                    <h3 class="producto-titulo">${producto.titulo}</h3>
-                    <p class="producto-precio">$${producto.precio}</p>
+                    <h3 class="producto-titulo">${producto-titulo}</h3>
+                    <p class="producto-precio">$${producto-precio}</p>
                     <b<utton class="producto-agregar" id="${producto.id}agregar</button>
                 </div>
                 
@@ -100,6 +100,24 @@ const productos [
 
              cargarProductos();
 
+             botonesCategorias.forEach (boton => {
+                boton.addEventListener ("click",(e) => {
+                    botonesCategorias.forEach (boton => boton.classList.remove("active"));
+                    e.currentTarget.classList.add("active");
+
+                    const productosBoton = productos.filter ( producto =>producto.categoria.id === e.currentTarget.id)
+                }
+                
+                )
+             }
+                
+                )
+
+
+
+
+
+
              const productosEnCarrito = [;
                 function agregarAlcarrito (e) {
                     const IdBoton =e.productosEnCarrito
@@ -108,6 +126,15 @@ const productos [
 
               ]
 
+
+
+            //   <div class="producto">
+            //     <img class="producto-imagen" src="./img/iphone-12.png" alt="imagencelular">
+            //     <div class="caracteristicas" >
+            //         <h3 class="producto-titulo"> iphone 12</h3>
+            //         <p class="producto-precio">$1000</p>
+            //         <button class="producto-agregar"> agregar</button>
+            //     </div>
 
 
              
